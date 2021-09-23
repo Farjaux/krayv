f8d0e64c8529a0740d00a6ae2f26b2fd2f074acd
Farjaux <mttmcca@gmail.com>
2021-09-23 16:21:55 -0700

    finsihed pug, added post shuffle

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,13 +4,30 @@ const catchAsync = require('../utils/catchAsync');
 exports.getPosts = catchAsync(async (req, res) => {
   // 1) Get post data from collection
   const restaurants = await Restaurants.find();
-  const posts = [];
+  const unshuffledPosts = [];
   for (let i = 0; i < restaurants.length; i++) {
     for (let p = 0; p < restaurants[i].posts.length; p++) {
       const allPosts = restaurants[i].posts[p];
-      posts.push(allPosts);
+      unshuffledPosts.push(allPosts);
     }
   }
+  // Shuffle posts to show in random order (knuth-shuffle)
+  const shuffledPosts = function (array) {
+    let currentIndex = array.length;
+    let randomIndex;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+    return array;
+  };
+  const posts = shuffledPosts(unshuffledPosts);
+
   // 2) Build Template
 
   // 3) Render that template using post data from 1
